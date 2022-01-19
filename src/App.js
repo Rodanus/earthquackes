18296a0343ddabcfc972c83e8231f6e54cd07ed4
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2022-01-19 18:06:28 +0300

    feat: Use react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 import "./App.css";
+import Details from "./Pages/Details/Details";
 import Homepage from "./Pages/Homepage/Homepage";
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/details/:eventId" element={<Details />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
