bdc7432cd976a22d92873494d2ecb51128692f1c
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2022-01-20 06:03:48 +0300

    feat: Fetch event details on click

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -1,8 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import "./Details.css";
 
-function Details(props) {
-  return <div>Details</div>;
+function Details() {
+  const [eventDetails, setEventDetails] = useState({});
+  const [eventDate, setEventDate] = useState("");
+  const { eventId } = useParams();
+
+  useEffect(() => {
+    fetch(
+      `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventid=${eventId}`
+    )
+      .then(res => res.json())
+      .then(res => {
+        const { title, eventtime, magnitude, latitude, longitude } =
+          res.properties.products.origin[0].properties;
+        const eventDate = new Date(eventtime);
+        setEventDate(eventDate.toLocaleString());
+        setEventDetails({ title, magnitude, latitude, longitude });
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>{eventDetails.title}</h1>
+      <h2>{eventDate}</h2>
+
+      <div className="event-extra-details">
+        <div className="event-extra-detail">
+          <h3 className="event-extra-detail-title">magnitude</h3>
+          <h4 className="event-extra-detail-value">{eventDetails.magnitude}</h4>
+        </div>
+        <div className="event-extra-detail">
+          <h3 className="event-extra-detail-title">latitude</h3>
+          <h4 className="event-extra-detail-value">{eventDetails.latitude}</h4>
+        </div>
+        <div className="event-extra-detail">
+          <h3 className="event-extra-detail-title">longitude</h3>
+          <h4 className="event-extra-detail-value">{eventDetails.longitude}</h4>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Details;
